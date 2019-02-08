0666315dc15a7094a7c8d6febf48978974beadb0
zcybupt <jp2016213431@qmuk.ac.uk>
2019-02-08 16:23:40 +0800

    完成文件拖放

--- RFB_GUI.py
+++ RFB_GUI.py
@@ -29,16 +29,11 @@ class RFB_GUI(QtWidgets.QMainWindow):
 
         self.file_item = QtWidgets.QAction('Open image', self)
         self.file_item.setShortcut('Ctrl+O')
-        self.file_item.setStatusTip('Open new file')
         self.file_item.triggered.connect(self.select_file)
-        # self.file = self.menuBar().addMenu('File')
-        # self.file.addAction(self.file_item)
 
-        self.label = QLabel(self)
-        self.label.setText("Please drag image here\nor\nPress Ctrl+O to select")
-        self.label.setAlignment(Qt.AlignCenter)
-        self.label.setFont(QtGui.QFont("Ubuntu Mono", 30))
+        self.label = self.DragLabel("Please drag image here\nor\nPress Ctrl+O to select", self)
         self.label.addAction(self.file_item)
+
         self.setCentralWidget(self.label)
 
         self.cfg = VOC_300
@@ -72,26 +67,30 @@ class RFB_GUI(QtWidgets.QMainWindow):
                                                           "Image files(*.bmp *.jpg *.pbm *.pgm *.png *.ppm *.xbm *.xpm)"
                                                           ";;All files (*.*)")
         # try:
-        self.detect(file_path[0])
+        self.detect(file_path[0], self)
         # except Exception as e:
         #     QtWidgets.QMessageBox.information(self, "Alert", str(e))
 
-    def detect(self, file_name):
+    def detect(self, file_name, object):
+        print(file_name)
         start_time = time.time()
-        img = cv2.imread(file_name)
+        img = cv2.imread(file_name.strip())
+        if img is None:
+            QtWidgets.QMessageBox.information(self, "Alert", "Please select images")
+            return
         scale = torch.Tensor([img.shape[1], img.shape[0],
                               img.shape[1], img.shape[0]])
-        detector = Detect(self.numclass, 0, self.cfg)
-        transform = BaseTransform(self.net.size, (123, 117, 104), (2, 0, 1))
+        detector = Detect(object.numclass, 0, object.cfg)
+        transform = BaseTransform(object.net.size, (123, 117, 104), (2, 0, 1))
         with torch.no_grad():
             x = transform(img).unsqueeze(0)
-            if self.cuda:
+            if object.cuda:
                 x = x.cuda()
                 scale = scale.cuda()
-        out = self.net(x)
+        out = object.net(x)
         with torch.no_grad():
-            priors = self.priorbox.forward()
-            if self.cuda:
+            priors = object.priorbox.forward()
+            if object.cuda:
                 priors = priors.cuda()
         boxes, scores = detector.forward(out, priors)
         boxes = boxes[0]
@@ -99,12 +98,9 @@ class RFB_GUI(QtWidgets.QMainWindow):
         boxes *= scale
         boxes = boxes.cpu().numpy()
         scores = scores.cpu().numpy()
-        # Create figure and axes
-        # Display the image
 
-        # scale each detection back up to the image
         result_set = []
-        for j in range(1, self.numclass):
+        for j in range(1, object.numclass):
             max_ = max(scores[:, j])
             inds = np.where(scores[:, j] > 0.2)[0]  # conf > 0.6
             if inds is None:
@@ -113,7 +109,7 @@ class RFB_GUI(QtWidgets.QMainWindow):
             c_scores = scores[inds, j]
             c_dets = np.hstack((c_bboxes, c_scores[:, np.newaxis])).astype(
                 np.float32, copy=False)
-            keep = self.nms_py(c_dets, 0.6)
+            keep = object.nms_py(c_dets, 0.6)
             c_dets = c_dets[keep, :]
             c_bboxes = c_dets[:, :4]
             for bbox in c_bboxes:
@@ -127,12 +123,11 @@ class RFB_GUI(QtWidgets.QMainWindow):
         end_time = time.time()
         print(end_time - start_time)
         img_data = QtGui.QPixmap("my_test.png")
-        height = self.height()
-        width = self.height() / img_data.height() * img_data.width()
+        height = object.height()
+        width = object.height() / img_data.height() * img_data.width()
         img_data = img_data.scaled(width, height)
-        self.label.resize(width, height)
-
-        self.label.setPixmap(img_data)
+        object.label.resize(width, height)
+        object.label.setPixmap(img_data)
 
     def nms_py(self, dets, thresh):
         x1 = dets[:, 0]
@@ -171,6 +166,20 @@ class RFB_GUI(QtWidgets.QMainWindow):
                 if ovr >= thresh:
                     suppressed[j] = 1
         return keep
+
+    class DragLabel(QLabel):
+        def __init__(self, text, parent):
+            super().__init__(text, parent)
+            self.parent = parent
+            self.setAcceptDrops(True)
+            self.setFont(QtGui.QFont("Ubuntu Mono", 30))
+            self.setAlignment(Qt.AlignCenter)
+
+        def dragEnterEvent(self, QDragEnterEvent):
+            QDragEnterEvent.accept()
+
+        def dropEvent(self, QDropEvent):
+            self.parent.detect(QDropEvent.mimeData().text()[7:], self.parent)
 
 
 if __name__ == '__main__':
